8674954cf125c5d2d4035d8c1eb996553f68e17d
Besac1 <cholletax@orange.fr>
2015-07-24 23:06:33 +0200

    Modifications du script js

--- UI-My_interface/JS/script.js
+++ UI-My_interface/JS/script.js
@@ -1,15 +1,72 @@
 document.addEventListener("DOMContentLoaded", function () {
-	
-	var Main = {
 
-		init: function () {
+    'use strict';
+    var Main = {
 
-			return 'lol';
+        init: function () {
 
-		}
+            var choix = document.getElementsByClassName("choix"),
+                mainpage = document.getElementById("mainpage"),
+                context = document.getElementById("context"),
+                that = this;
 
-	};
+            mainpage.addEventListener("contextmenu", function (e) {
 
-	Main.init();
+            e.preventDefault();
+
+                context.style.display = "block";
+                context.style.top = (e.pageY - 10) + "px";
+                context.style.left = (e.pageX - 10) + "px";
+
+            });
+
+
+            for (var i = choix.length - 1; i >= 0; i--) {
+
+
+                choix[i].addEventListener('click', function () {
+
+                    that.do(this.value);
+
+                });
+
+            };
+
+
+            mainpage.addEventListener("click", function (e) {
+
+                context.style.top = "";
+                context.style.left = "";
+                context.style.display = "";
+
+            });
+
+        },
+
+        do: function (e) {
+
+            switch(e) {
+
+                case 1:
+
+                    console.log("mkdir");
+                    break;
+
+                case 2:
+
+                    console.log("lolilol");
+                    break;
+
+                default:
+
+                    return false;
+
+            }
+
+        }
+
+    };
+
+    Main.init();
 
 });
