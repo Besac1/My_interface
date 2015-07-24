2e819818a5d6388a2970b83cb6a894a1f62c578b
Besac1 <cholletax@orange.fr>
2015-07-24 23:09:12 +0200

    Espacement des balises

--- index.php
+++ index.php
@@ -1,13 +1,20 @@
 <?php require 'UI-My_interface/config.php'; ?>
 <?php require 'UI-My_interface/interface.php'; ?>
 <!DOCTYPE html>
-<html>
+
+<html lang = 'fr'>
+
 <head>
+
 	<title>My_interface</title>
 	<link rel="stylesheet" type="text/css" href="UI-My_interface/style.css">
 	<meta charset = "UTF-8"/>
+
 </head>
+
+
 <body>
+
 	<?php var_dump($files); ?>
 	
 	<section id = "mainpage">
@@ -27,5 +34,7 @@
 	</section>
 
     <script type = "text/javascript" src = "UI-My_interface/JS/script.js"></script>
+
 </body>
+
 </html>
